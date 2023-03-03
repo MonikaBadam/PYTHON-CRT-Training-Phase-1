b330825cff6028e1c2bebfe7bde41d7ce6b3474a
MonikaBadam <126134987+MonikaBadam@users.noreply.github.com>
2023-03-03 19:06:04 +0530

    initial commit

--- string formats.py
+++ string formats.py
@@ -16,25 +16,28 @@ id="Abc1001"'''
 
 # create a method
 
-'''class car:
-    name="bmw"
-    color="black"
+'''car={
+    "name":"bmw",
+    "color":"black"
+    }
 
-    
-def print_details(self):
-    print(f"name: {self.name} and roll: {self.color}")
-
-
-ob1= car()
-ob1.print_details()'''
-
-
+print(car)
+car.update(
+    {"comfort":"yes"})
+print(car)
+print(car["color"])
+print(car["name"])
+print(car.keys())
+print(car.values())
+print(car.items())
+car['color']='white'
+print(car)'''
 
 #constructor syntax
 
 '''def __init__(self):'''
 
-class student:
+'''class student:
     def __init__(self,name,roll):
         self.s_name=name
         self.s_roll=roll
@@ -46,7 +49,27 @@ class student:
 #import sample
 
 a=sample.A.say_hello()
-b=sample.B.say_hello()
+b=sample.B.say_hello()'''
+
+
+#self keyword
+
+'''class student:
+    name = "monika"
+    roll= 1001
+
+
+    def study(self):
+        print(f" {self.name} is studying")
+
+
+var1= student()
+print(var1.name)
+var1.study()'''
+
+
+
+
 
 
 
